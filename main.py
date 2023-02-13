3700a129cce86cf94d632a2962bc7266aff46a5b
Eve <jam137@aber.ac.uk>
2023-02-13 23:25:22 +0000

    pre-meeting

--- main.py
+++ main.py
@@ -21,46 +21,51 @@ def remove_tags(tag_name, root):
     #         if (n.attrib == tag):
     #             n.remove()
 
-def xml_to_alphanumeric(file_name):
-    root = ET.parse("xml/" + file_name + ".xml").getroot()
-    tags_to_delete = ["col"]
-    # remove_tags(tags_to_delete, root)
-
-    for tag in tags_to_delete:
-        remove_tags(tag, root)
-
+def process_strings(root):
     str = ""
-    hyphenated = False # search "proposed plan per cents at d shews" in 08 output to check
+    hyphenated = False
 
     for n in root.itertext():
 
         if (hyphenated):
-            init = "" #work this out
+            init = "<hyphenated>" #set tag where word is split between lines
             hyphenated = False
         else:
-            init = " "
+            init = ""
         temp = init + n
 
         temp = temp.replace("—", " ")
         if ("-\n" in temp):
             temp = temp.replace("-\n", "")
             hyphenated = True
-        temp = re.sub("[^a-zA-Z \n-]", "", temp).lower()
-        # temp = temp.replace("\n", "")
-        # temp = re.sub('s+', ' ', temp)
+        temp = temp.replace("<hyphenated>\n", "") #remove tag
+
+        temp = re.sub("[^a-zA-Z. \n-]", "", temp).lower()
+        temp = re.sub("[^]")
+        # temp = re.sub(r'(?:^|\s)(\w)\.', r'\1', temp)
         str += temp
 
-    str = " ".join(str.split()) #works but seems relatively inefficient as far as options go
+    return "".join(str.split()) #works but seems relatively inefficient as far as options go
+
+def preprocess_xml(file_name):
+    root = ET.parse("xml/" + file_name + ".xml").getroot()
+    tags_to_delete = ["col", "title", "td"] #all tags to be deleted
+    remove_tags(tags_to_delete, root)
+
+    for tag in tags_to_delete:
+        remove_tags(tag, root)
+
+    str = process_strings(root)
     return str
 
 def load_files():
     all_files = ["S1V0008P0"] #todo change back to 1
         # ,"S1V0003P0","S1V0004P0","S1V0005P0","S1V0006P0","S1V0007P0","S1V0008P0","S1V0009P0_a",
-        #          "S1V0009P0_b","S1V0010P0","S1V0011P0","S1V0012P0","S1V0013P0","S1V0014P0","S1V0015P0","S1V0019P0",
-        #          "S1V0020P0"]
+        # "S1V0009P0_b","S1V0010P0","S1V0011P0","S1V0012P0","S1V0013P0","S1V0014P0","S1V0015P0","S1V0019P0",
+        # "S1V0020P0"]
 
     for n in all_files:
-        xml_string = xml_to_alphanumeric(n)
+        xml_string = preprocess_xml(n)
         print(xml_string)
 
 if __name__ == '__main__':
