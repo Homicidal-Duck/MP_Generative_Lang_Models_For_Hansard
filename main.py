13e724adb0db7edc57f9b21e8d225804ec39c02d
Eve <jam137@aber.ac.uk>
2023-02-13 06:00:00 +0000

    update to broken word checking

--- main.py
+++ main.py
@@ -1,26 +1,56 @@
 import xml.etree.ElementTree as ET
 import re
 
-
-
-
 def main():
     load_files()
 
 
+def remove_tags(tag_name, root):
+    for n in root.iter(tag_name):
+        n.text = "" #seems to work??
+        # print(n.text)
+        # n.clear()
+
+    # input() #for waiting to investigate outputs of attribute
+
+    # for n in root.findall('{http://www.w3.org/2001/XMLSchema-instance}col'):
+    #     n.remove()
+
+    # for n in root.iter():
+    #     for tag in tag_names:
+    #         if (n.attrib == tag):
+    #             n.remove()
+
 def xml_to_alphanumeric(file_name):
     root = ET.parse("xml/" + file_name + ".xml").getroot()
-    str = ""
-    hyphenated = ""
-    for n in root.itertext():
-        temp = " " + n
-        temp = temp.replace("—", " ")
-        temp = temp.replace("-\n", "")
-        temp = re.sub("[^a-zA-Z \n-]", "", temp).lower()
-        temp = temp.replace("\n", "")
-        temp = re.sub("\\s+", " ", temp)
+    tags_to_delete = ["col"]
+    # remove_tags(tags_to_delete, root)
 
+    for tag in tags_to_delete:
+        remove_tags(tag, root)
+
+    str = ""
+    hyphenated = False # search "proposed plan per cents at d shews" in 08 output to check
+
+    for n in root.itertext():
+
+        if (hyphenated):
+            init = "" #work this out
+            hyphenated = False
+        else:
+            init = " "
+        temp = init + n
+
+        temp = temp.replace("—", " ")
+        if ("-\n" in temp):
+            temp = temp.replace("-\n", "")
+            hyphenated = True
+        temp = re.sub("[^a-zA-Z \n-]", "", temp).lower()
+        # temp = temp.replace("\n", "")
+        # temp = re.sub('s+', ' ', temp)
         str += temp
+
+    str = " ".join(str.split()) #works but seems relatively inefficient as far as options go
     return str
 
 def load_files():
