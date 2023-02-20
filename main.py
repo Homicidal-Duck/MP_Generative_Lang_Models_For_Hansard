1c670685a59b1ee33aa7cce0bd87fc6c0b0dccb6
Eve <jam137@aber.ac.uk>
2023-02-20 23:43:52 +0000

    added 2D dictionary and 2D array - latter needs refining

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import time
+
 import preprocess
 import model
 import torch
 
 
 def main():
+    start = time.perf_counter()
+
     all_files = ["S1V0001P0", "S1V0003P0", "S1V0004P0", "S1V0005P0", "S1V0006P0", "S1V0007P0", "S1V0008P0",
                  "S1V0009P0_a", "S1V0009P0_b", "S1V0010P0", "S1V0011P0", "S1V0012P0", "S1V0013P0", "S1V0014P0",
                  "S1V0015P0", "S1V0019P0", "S1V0020P0"]
@@ -16,8 +20,11 @@ def main():
         # word_counts = model.count_words(preprocessed, word_counts)
         bigram_counts = model.count_bigrams(preprocessed, bigram_counts)
 
-    #Build 2D dictionary
-    model.init_2D_dict(bigram_counts)
+    # Build 2D dictionary
+    bigram_2D_dict = model.dict2D_from_dict(bigram_counts)
+
+    # Build 2D array TAKES A LONG TIME - make dense
+    print(model.array_from_dict(bigram_counts))
 
     # Print word counts to file
     # word_count_file = open("output/word_counts.txt", "w")
@@ -29,6 +36,9 @@ def main():
     # for w in sorted(bigram_counts, key=bigram_counts.get, reverse=True):
     #     if(bigram_counts[w] > 3):
     #         bigram_count_file.write(str(w) + ' : ' + str(bigram_counts[w]) + '\n')
+
+    end = time.perf_counter()
+    print(f"{end - start:0.4f} seconds")
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -18,19 +18,50 @@ def count_bigrams(string, bigram_counts):
     return bigram_counts
 
 
-def init_2D_dict(bigram_counts):
+def dict2D_from_dict(bigram_counts):
     # for bigram in bigram_counts.items():
     #     print(bigram[0][0])
     #       bigram[0][0]
 
-    # bigram_2D = torch.zeros(len(bigram_counts), len(bigram_counts), dtype=torch.int32)
     bigram_2D_dict = {}
     bigrams_sorted = sorted(bigram_counts, key=bigram_counts.get, reverse=True)
     for bigram in bigrams_sorted:
-        bigram_2D_dict.update({bigram[0]: dict()}) #initialise dictionaries
+        bigram_2D_dict.update({bigram[0]: dict()})  # initialise dictionaries
 
     for bigram in bigrams_sorted:
         count = bigram_counts.get(bigram)
-        bigram_2D_dict.get(bigram[0]).update({bigram[1]: count}) #set value
+        bigram_2D_dict.get(bigram[0]).update({bigram[1]: count})  # set value
 
-    input()
+    return bigram_2D_dict
+    # input()
+
+
+# def array_from_dict(bigram_2D_dict, bigram_counts):
+#     # bigram_2D_arr = torch.zeros(len(bigram_2D_dict), len(bigram_2D_dict), dtype=torch.int32)
+#     # bigrams_sorted = sorted(bigram_counts, key=bigram_counts.get, reverse=True)
+#     print(bigram_2D_dict)
+#
+#     tensor_bigrams = []
+#     for row in bigram_2D_dict:
+#         tensor_row = torch.tensor(list(row[1].values()))
+#         tensor_bigrams.append(tensor_row)
+#
+#     padded_tensor_bigrams = torch.pad_sequence(tensor_bigrams, batch_first=True)
+#     print(padded_tensor_bigrams)
+
+
+def array_from_dict(bigram_counts):
+    # Get indices from keys
+    row_indices = list(set([k[0] for k in bigram_counts.keys()]))
+    col_indices = list(set([k[1] for k in bigram_counts.keys()]))
+
+    # Create a 2D PyTorch tensor
+    tensor_bigrams = torch.zeros(len(row_indices), len(col_indices))
+
+    # Fill in tensor with vals from bigram_counts
+    for key, val in bigram_counts.items():
+        row_index = row_indices.index(key[0])
+        col_index = col_indices.index(key[1])
+        tensor_bigrams[row_index, col_index] = val
+
+    return tensor_bigrams
